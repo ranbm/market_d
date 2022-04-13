0dff9a89b6a9bd0d57dfd5ae1f57436518a2e897
ranbm <ran.ben.melech1@gmail.com>
2022-04-13 16:30:55 +0300

    dockerized

---
+++ web/run.py
@@ -0,0 +1,7 @@
+from market import app
+import os
+
+
+#Checks if the run.py file has executed directly and not imported
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=os.environ.get('DEBUG') == '1')
